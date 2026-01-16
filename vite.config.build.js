375608004a9edb0b40f28e6fc5e313f5f3224c27
Juan A. Martínez <jamartinezv@godoservices.com>
2026-01-16 11:58:09 +0100

    chore: create config for build

---
+++ vite.config.build.js
@@ -0,0 +1,25 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig(() => {
+	return {
+		resolve: {
+			alias: {
+				"#": resolve(__dirname, "./tests"),
+				"@": resolve(__dirname, "./src"),
+			},
+		},
+		build: {
+			outDir: "./dist/lib",
+			lib: {
+				entry: resolve(__dirname, "src/index.js"),
+				name: "dai",
+				formats: ["es", "cjs", "umd"],
+				fileName: "dai",
+			},
+			emptyOutDir: false,
+			minify: true,
+			sourcemap: false,
+		},
+	};
+});
